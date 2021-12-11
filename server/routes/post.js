a5ef2c67ca4fc629ccd553937d1c29955be23cdb
quygcd18687 <66302075+quygcd18687@users.noreply.github.com>
2021-12-11 19:11:16 +0700

    Create PUT api data

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,48 +1,95 @@
-const express = require('express')
-const router = express.Router()
-const verifyToken = require('../middleware/Auth')
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middleware/Auth");
 
-const Post = require('../models/Post')
+const Post = require("../models/Post");
 
 // @route GET api/posts
 // @desc Get posts
 // @access Private
-router.get('/', verifyToken, async (req, res) => {
-	try {
-		const posts = await Post.find({ user: req.userId }).populate('user',['username','createAt'])
-		res.json({ success: true, posts })
-	} catch (error) {
-		console.log(error)
-		res.status(500).json({ success: false, message: 'Internal server error' })
-	}
-})
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.userId }).populate("user", [
+      "username",
+      "createAt",
+    ]);
+    res.json({ success: true, posts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
 
 //@route POST api/post
 //@desc Create post
 //@acces  private
 
-router.post('/', verifyToken, async (req, res) => {
-    const { title, description, url, img } = req.body
+router.post("/", verifyToken, async (req, res) => {
+  const { title, description, url, img } = req.body;
 
-    //valiation
-    if (!title)
-        return res.status(400).json({ success: false, message: 'title is require' })
+  //valiation
+  if (!title)
+    return res
+      .status(400)
+      .json({ success: false, message: "title is require" });
 
-    try {
-        const newPost = new Post({ 
-            title, 
-            description, 
-            url: url.startsWith('https://') ? url : `https://${url}`, 
-            img ,
-            user: req.userId
-        
-        })
-        await newPost.save()
+  try {
+    const newPost = new Post({
+      title,
+      description,
+      url: url.startsWith("https://") ? url : `https://${url}`,
+      img,
+      user: req.userId,
+    });
+    await newPost.save();
 
-        res.json({success: true, message: 'okokkokokokokokokoko', post: newPost})
-    } catch (error) {
-        console.log(error)
-		res.status(500).json({ success: false, message: 'Internal server error' })
-    }
-})
-module.exports = router
+    res.json({ success: true, message: "okokkokokokokokokoko", post: newPost });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+// @route PUT api/posts
+// @desc PUT post
+// @access Private
+router.put("/:id", verifyToken, async (req, res) => {
+  const { title, description, url, img } = req.body;
+
+  try {
+    let updatedPost = {
+      title,
+      description: description || "",
+      url: (url.startsWith("https://") ? url : `https://${url}`) || "",
+      img: img || "",
+    };
+
+    const postUpdateCondition = { _id: req.params.id, user: req.userId };
+
+    updatedPost = await Post.findOneAndUpdate(
+      postUpdateCondition,
+      updatedPost,
+      { new: true }
+    );
+
+    // User not authorised to update post or post not found
+    if (!updatedPost)
+      return res
+        .status(401)
+        .json({
+          success: false,
+          message: "Post not found or user not authorised",
+        });
+
+    res.json({
+      success: true,
+      message: "Excellent progress!",
+      post: updatedPost,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+module.exports = router;
